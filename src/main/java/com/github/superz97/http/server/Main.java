873aa3d8567f7668f117d1c101a2e3da45b2ada5
superz97 <b.zangar15@gmail.com>
2025-09-19 20:11:46 +0500

    Implement file upload/download functionality

--- src/main/java/com/github/superz97/http/server/Main.java
+++ src/main/java/com/github/superz97/http/server/Main.java
@@ -1,8 +1,11 @@
 package com.github.superz97.http.server;
 
+import com.github.superz97.http.server.common.ApplicationParameters;
+
 public class Main {
 
     public static void main(String[] args) {
+        ApplicationParameters.getInstance().setFileDirectory(args);
         var server = new Server(4221);
         server.start();
     }
--- src/main/java/com/github/superz97/http/server/controller/ApplicationController.java
+++ src/main/java/com/github/superz97/http/server/controller/ApplicationController.java
@@ -1,12 +1,19 @@
 package com.github.superz97.http.server.controller;
 
 import com.github.superz97.http.server.bind.RequestMapping;
+import com.github.superz97.http.server.common.ApplicationParameters;
 import com.github.superz97.http.server.common.HttpHeaders;
 import com.github.superz97.http.server.common.HttpMethod;
 import com.github.superz97.http.server.common.HttpStatus;
 import com.github.superz97.http.server.request.RequestContext;
 import com.github.superz97.http.server.response.ResponseContext;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class ApplicationController {
@@ -35,6 +42,59 @@ public class ApplicationController {
                 responseBody);
     }
 
+    @RequestMapping(path = "/files/{file}", method = HttpMethod.POST)
+    public ResponseContext saveFile(RequestContext context) {
+        if (!ApplicationParameters.getInstance().isDirectoryExists()) {
+            return ResponseContext.build(HttpStatus.NOT_FOUND);
+        }
+        var directory =  ApplicationParameters.getInstance().getFileDirectory();
+        directory = directory.endsWith("/") ? directory : directory + '/';
+        var fileName = context.getLastPart();
+        saveFile(String.format("%s%s", directory, fileName), context.getBody());
+        return ResponseContext.build(HttpStatus.CREATED);
+    }
 
+    @RequestMapping(path = "/files/{file}", method = HttpMethod.GET)
+    public ResponseContext readFileByName(RequestContext context) {
+        if (!ApplicationParameters.getInstance().isDirectoryExists()) {
+            return ResponseContext.build(HttpStatus.NOT_FOUND);
+        }
+        var directory = ApplicationParameters.getInstance().getFileDirectory();
+        directory = directory.endsWith("/") ? directory : directory + '/';
+        var fileName = context.getLastPart();
+        var file = new File(String.format("%s%s", directory, fileName));
+        if (!file.exists() || !file.isFile()) {
+            return ResponseContext.build(HttpStatus.NOT_FOUND);
+        }
+        String fileContent = readFile(file);
+        return ResponseContext.build(
+                HttpStatus.OK,
+                HttpHeaders.fromHeaderMap(Map.of("Content-Type", "application/octet-stream",
+                        "Content-Length", String.valueOf(fileContent.getBytes().length))),
+                fileContent
+        );
+    }
+
+    private String readFile(File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStream.read(bytes);
+            return new String(bytes);
+        } catch (IOException ex) {
+            throw new RuntimeException("Error reading file " + file.getAbsolutePath(), ex);
+        }
+    }
+
+    private void saveFile(String fullPath, String content) {
+        try {
+            Path path = Paths.get(fullPath);
+            if (Files.exists(path)) {
+                Files.delete(path);
+            }
+            Files.write(path, content.getBytes());
+        } catch (IOException ex) {
+            throw new RuntimeException("Exception trying to save file", ex);
+        }
+    }
 
 }
